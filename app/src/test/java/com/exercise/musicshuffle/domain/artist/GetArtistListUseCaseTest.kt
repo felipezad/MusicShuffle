02b46a52794bd7c61c4ef56e7f1821190f85e308
Felipe <felipezad@gmail.com>
2019-05-31 15:22:27 -0300

    Create unit tes for ArtistList UseCase

---
+++ app/src/test/java/com/exercise/musicshuffle/domain/artist/GetArtistListUseCaseTest.kt
@@ -0,0 +1,46 @@
+package com.exercise.musicshuffle.domain.artist
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import io.reactivex.Observable
+import io.reactivex.Single
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito
+
+class GetArtistListUseCaseTest {
+
+    @Rule
+    @JvmField
+    val rule = InstantTaskExecutorRule()
+
+    private lateinit var musicRepositoryMock: MusicRepository
+    private lateinit var getArtistListUseCase: GetArtistListUseCase
+
+    @Before
+    fun setUp() {
+        musicRepositoryMock = Mockito.mock(MusicRepository::class.java)
+        getArtistListUseCase = GetArtistListUseCase(musicRepository = musicRepositoryMock)
+
+    }
+
+    @Test
+    fun executeGetArtistListUseCaseTest() {
+        val musicMock = Music(
+            id = 1,
+            artistId = 1,
+            artistName = "artistNameMock",
+            primaryGenre = "genreMock",
+            trackName = "trackNameMock"
+        )
+        val responseFromRepository = listOf(musicMock)
+        val responseFromUseCase = GetArtistListUseCase.Result.Success(listOf(musicMock)) as GetArtistListUseCase.Result
+        Mockito.`when`(musicRepositoryMock.getArtist(Mockito.anyString()))
+            .thenReturn(Single.just(responseFromRepository))
+        val responseFromExecute: Observable<GetArtistListUseCase.Result> = getArtistListUseCase.execute("any_string")
+        responseFromExecute.subscribe({
+            assertEquals(responseFromUseCase, responseFromExecute)
+        }, {})
+    }
+}
