755ae490fc5619508d60c6e5efa42d077bada1de
Felipe Zadinello <felipezad@gmail.com>
2019-06-02 12:23:52 -0300

    Create function to shuffle musics

--- app/src/main/java/com/exercise/musicshuffle/application/MainViewModel.kt
+++ app/src/main/java/com/exercise/musicshuffle/application/MainViewModel.kt
@@ -8,30 +8,18 @@ import io.reactivex.Scheduler
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import kotlin.random.Random
 
 class MainViewModel(
     private val getArtistListUseCase: GetArtistListUseCase,
     private val subscribeOn: Scheduler = Schedulers.io(),
     private val observeOn: Scheduler = AndroidSchedulers.mainThread(),
-    val artistList: MutableLiveData<List<Music>> = MutableLiveData()
+    val artistList: MutableLiveData<List<Music>> = MutableLiveData(),
+    val enableShuffleButton: MutableLiveData<Boolean> = MutableLiveData()
 ) : ViewModel() {
 
 
     private val disposables = CompositeDisposable()
-
-    fun getArtistList(artistId: String) {
-        disposables.add(
-            getArtistListUseCase
-                .execute(artistId = artistId)
-                .subscribeOn(subscribeOn)
-                .observeOn(observeOn)
-                .subscribe(this::handleGetArtistListSuccess)
-        )
-    }
-
-    fun destroy() {
-        disposables.clear()
-    }
 
     private fun handleGetArtistListSuccess(result: GetArtistListUseCase.Result) {
         when (result) {
@@ -43,6 +31,29 @@ class MainViewModel(
             }
         }
     }
+
+    fun getArtistList(artistId: String) {
+        disposables.add(
+            getArtistListUseCase
+                .execute(artistId = artistId)
+                .subscribeOn(subscribeOn)
+                .observeOn(observeOn)
+                .subscribe(this::handleGetArtistListSuccess)
+        )
+    }
+
+    fun shuffleArtistList() {
+        artistList.value?.let {
+            val toMutableList = it.toMutableList()
+            artistList.postValue(toMutableList.shuffleMusicList())
+            enableShuffleButton.postValue(true)
+        }
+    }
+
+    fun destroy() {
+        disposables.clear()
+    }
+
 
     class MainViewModelFactory(
         private val getArtistListUseCase: GetArtistListUseCase = GetArtistListUseCase(),
@@ -60,4 +71,26 @@ class MainViewModel(
         }
 
     }
+
+    private fun MutableList<Music>.shuffleMusicList(): List<Music> {
+        val shuffleList: MutableList<Music> = mutableListOf()
+        while (this.size > 0) {
+            val index = Random(System.currentTimeMillis()).nextInt(this.size)
+            val getRandomMusic = this[index]
+            if (shuffleList.size == 0) {
+                shuffleList.add(this.removeAt(index))
+            } else {
+                shuffleList.lastOrNull {
+                    if (it.artistName.equals(other = getRandomMusic.artistName, ignoreCase = true)) {
+                        false
+                    } else {
+                        shuffleList.add(this.removeAt(index))
+                        true
+                    }
+                }
+            }
+        }
+        return shuffleList
+    }
+
 }
