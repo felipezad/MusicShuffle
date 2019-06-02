90a2a21abc99c4097b881c06bfc7bcfe2474fadc
Felipe Zadinello <felipezad@gmail.com>
2019-06-02 14:27:38 -0300

    Create welcome activity

---
+++ app/src/main/java/com/exercise/musicshuffle/application/WelcomeActivity.kt
@@ -0,0 +1,15 @@
+package com.exercise.musicshuffle.application
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.exercise.musicshuffle.R
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+    }
+
+}
