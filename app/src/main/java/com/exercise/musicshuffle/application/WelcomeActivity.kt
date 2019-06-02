d6c4dc98204a10385f70a1d8e43d749520ea70a5
Felipe Zadinello <felipezad@gmail.com>
2019-06-02 15:06:42 -0300

    Create basic animation for activity transitioning

--- app/src/main/java/com/exercise/musicshuffle/application/WelcomeActivity.kt
+++ app/src/main/java/com/exercise/musicshuffle/application/WelcomeActivity.kt
@@ -1,15 +1,30 @@
 package com.exercise.musicshuffle.application
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.exercise.musicshuffle.R
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import java.util.concurrent.TimeUnit
 
 class WelcomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
-
+        createTimer().subscribe()
     }
 
+    private fun goToMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        overridePendingTransition(R.anim.abc_fade_in, R.anim.abc_fade_out)
+    }
+
+    private fun createTimer(): Observable<Long> {
+        return Observable.timer(3, TimeUnit.SECONDS)
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnNext { goToMainActivity() }
+
+    }
 }
